7032a2d96cc50b2935e68ad87cfc95a3459eb007
thepulkitgupta <55017145+thepulkitgupta@users.noreply.github.com>
2023-05-14 22:33:07 +0530

    ArrayList Done

---
+++ src/main/java/DataStructures/Lists/ArrayList.java
@@ -0,0 +1,57 @@
+package DataStructures.Lists;
+
+import Helpers.Employee;
+
+import java.util.List;
+
+public class ArrayList {
+    public static void main(String[]args){
+
+        List<Employee> employeeList = new java.util.ArrayList<>();
+        employeeList.add(new Employee("Jane","Jones",123));
+        employeeList.add(new Employee("John","Doe",4567));
+        employeeList.add(new Employee("Mary","Smith",22));
+        employeeList.add(new Employee("Mike","Wilson",3245));
+//        employeeList.forEach(employee -> System.out.println(employee));
+
+        //this is done in constant time, because arraylist is being backed by array
+        System.out.println(employeeList.get(1));
+
+        //check if list is empty
+        System.out.println(employeeList.isEmpty());
+
+        //set a new value to the employee
+        employeeList.set(1,new Employee("John","Adams",4568));
+//        employeeList.forEach(employee -> System.out.println(employee));
+
+        //get size of the list
+        System.out.println(employeeList.size());
+
+
+        //add element at a particular index
+        employeeList.add(3,new Employee("John","Doe",4567));
+//        employeeList.forEach(employee -> System.out.println(employee));
+
+        //get an array but we will get an object array
+        Object[] employeeArray = employeeList.toArray();
+        for(Object employee : employeeArray ){
+//            System.out.println("Employee : " + employee);
+        }
+        //or
+        Employee[] employeeNewArray = employeeList.toArray(new Employee[employeeList.size()]);
+        for(Employee employee : employeeNewArray ){
+            System.out.println("Employee : " + employee);
+        }
+
+        //check for an instance ( this will give false if equals is not implemented )
+        System.out.println(employeeList.contains(new Employee("Mary","Smith",22)));
+        //check of index of an element ( this will also work only if the equals is implemented)
+        System.out.println(employeeList.indexOf(new Employee("John","Doe",4567)));
+
+        //remove an element from the employee
+        employeeList.remove(2);
+        employeeList.forEach(employee -> System.out.println(employee));
+
+
+    }
+}
