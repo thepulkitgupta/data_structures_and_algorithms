5066bcb02b43cd8caa5ff6416de6803b76f20dc5
thepulkitgupta <55017145+thepulkitgupta@users.noreply.github.com>
2023-05-14 12:27:25 +0530

    Merge Sort Descending

--- src/main/java/Algorithms/Sort/MergeSort.java
+++ src/main/java/Algorithms/Sort/MergeSort.java
@@ -7,9 +7,41 @@ public class MergeSort {
     public static void main(String[]args){
         int[] intArray = {20, 35, -15, 7, 55,4,6 ,7,8, -1, 135, 15, 1, 1, -22};
 
-        mergeSort(intArray,0, intArray.length);
+//        mergeSort(intArray,0, intArray.length);
+        mergeSortDescending(intArray,0,intArray.length);
 
         print(intArray);
+    }
+
+    private static void mergeSortDescending(int[] intArray, int start, int end) {
+        //base case to stop the recursion
+        if(end-start <2)
+            return;
+
+        int mid=start + (end-start)/2;
+
+        mergeSortDescending(intArray,start,mid);
+        mergeSortDescending(intArray,mid,end);
+        mergeDescending(intArray, start,mid, end);
+    }
+
+    private static void mergeDescending(int[] input, int start,int mid, int end) {
+
+        if(input[mid-1]>input[mid]){
+            return;
+        }
+
+        int i=start;
+        int j=mid;
+        int tempIndex=0;
+        int[] tempArray = new int[end-start];
+        while(i<mid && j<end){
+            tempArray[tempIndex++]=input[i]>input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input,i,input,start+tempIndex,mid-i);
+
+        System.arraycopy(tempArray,0,input,start,tempIndex);
     }
 
     public static void mergeSort(int[]input, int start , int end){
@@ -68,6 +100,8 @@ public class MergeSort {
         System.arraycopy(temp, 0, input, start, tempIndex);
 
     }
+
+
 }
 /* NOTES
 1. It's a divide and conquer algorithm.
