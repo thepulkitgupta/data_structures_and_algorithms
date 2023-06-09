5e2e1157e7dddbf972c84bceb3e8468a3627adbc
thepulkitgupta <55017145+thepulkitgupta@users.noreply.github.com>
2023-06-09 15:36:43 +0530

    Linear Search Done

---
+++ src/main/java/Algorithms/Search/LinearSearch.java
@@ -0,0 +1,26 @@
+package Algorithms.Search;
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+        int[] intArray = {20,35,-15,7,55,1,-22};
+        System.out.println(linearSearch(intArray,-15));
+        System.out.println(linearSearch(intArray,1));
+        System.out.println(linearSearch(intArray,8888));
+        System.out.println(linearSearch(intArray,-22));
+
+
+    }
+
+    public static int linearSearch(int[]input, int value){
+        for (int i = 0; i < input.length; i++) {
+            if(input[i]==value)
+                return i;
+        }
+        return -1;
+    }
+}
+
+/* NOTES
+* 1. Worst case would be O(n) when item is at the very end.
+* */
